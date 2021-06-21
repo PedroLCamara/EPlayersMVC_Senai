6c65768cbb5bd9af8b31a2c263dac39907d10d0c
PedroLCamara <plcrlschool@gmail.com>
2021-06-21 16:48:39 -0300

    JuncaoComHTMLCSS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+        if (screen.width <= 767) {
+            function botaoDoMenu() {
+                    var menu = document.getElementById("menu_header");
+                    if (menu.style.display === "flex") {
+                        //esconder o menu
+                        menu.style.display = "none"
+                    } else {
+                        //mostrar o menu
+                        menu.style.display = "flex"
+                    }
+                }
+                function botaoDoMenuFechar() {
+                    var menu = document.getElementById("menu_header");
+                    if (menu.style.display === "flex") {
+                        //esconder o menu
+                        menu.style.display = "none"
+                    }
+                }
+            } else 
+        window.onresize = () => {
+            if (screen.width > 767) {
+                var menu = document.getElementById("menu_header");
+                menu.style.display = "flex";
+            } else{
+                menu.style.display = "none";
+            }
+        }
